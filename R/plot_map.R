dfb09a3e752b13a90c1b0b7b6cf4fcfe0662279a
rBatt <battrd@gmail.com>
2017-05-08 21:01:15 -0400

    allow zlim to standardize color scale in plot_map

--- R/plot_map.R
+++ R/plot_map.R
@@ -6,10 +6,11 @@
 #' @param y y coordinate
 #' @param marks value to be plotted
 #' @param owin object for outline
+#' @param zlim limits for the z (color) axis; useful for standardizing color scale across plots.
 #' 
 #' @return invisibly returns \code{NULL}
 #' @export
-plot_map <- function(x, y, marks, map_owin){
+plot_map <- function(x, y, marks, map_owin, zlim=NULL){
 	requireNamespace("maps", quietly = TRUE)
 	requireNamespace("raster", quietly = TRUE)
 	requireNamespace("spatstat", quietly = TRUE)
@@ -31,8 +32,11 @@ plot_map <- function(x, y, marks, map_owin){
 	mapPPP_ce <- spatstat::ppp(x=x, y=y, marks=marks, window=map_owin) 
 	t_idw <- spatstat::Smooth(mapPPP_ce, hmax=1)
 	z <- toRast(t_idw)
+	if(is.null(zlim)){
+		zlim <- range(z, na.rm=TRUE)
+	}
 	map_col <- grDevices::colorRampPalette(c("#000099", "#00FEFF", "#45FE4F", "#FCFF00", "#FF9400", "#FF3100"))(256)
-	raster::image(z, col=adjustcolor(map_col, 0.25), xlab="", ylab="", asp=1)
+	raster::image(z, col=adjustcolor(map_col, 0.25), xlab="", ylab="", asp=1, zlim=zlim)
 	if(any(x < -180)){
 		w2 <- maps::map('world2', plot=FALSE)
 		w2$x <- w2$x-360
