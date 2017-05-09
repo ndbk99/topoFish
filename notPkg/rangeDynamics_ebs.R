a55cdf603b853ea1dc6b27ad0f3c3f3521fdf4a0
rBatt <battrd@gmail.com>
2017-05-08 21:07:25 -0400

    add col/ext text; stdz btemp col; 1/2 circle radius

--- notPkg/rangeDynamics_ebs.R
+++ notPkg/rangeDynamics_ebs.R
@@ -24,14 +24,17 @@ uspp <- ebs_sppMaster[ce_categ=="both", unique(spp)]
 # setwd("~/Documents/School&Work/pinskyPost/topoFish/notPkg")
 old.wd <- getwd()
 setwd("../notPkg/figures")
-# for(us in 1:length(uspp)){
-for(us in 1:1){
+for(us in 1:length(uspp)){
+# for(us in 1:1){
 	tspp <- uspp[us]
 	treg <- 'ebs' # eastern bering sea
 	t_name <- paste0(treg, "_", gsub(" ", "_", tspp), ".gif") # file name
 	
+	col_yrs <- ebs_sppMaster[spp==tspp & col==1, sort(unique(year))]
+	ext_yrs <- ebs_sppMaster[spp==tspp & ext==1, sort(unique(year))]
+	
 	spp_dat <- ebs_dat[spp==tspp]
-	yr_range <- spp_dat[spp==tspp, range(year)]
+	yr_range <- spp_dat[, range(year)]
 	yr_dat <- ebs_dat[year>=yr_range[1] & year<=yr_range[2]]
 	uy <- yr_dat[,sort(unique(year))]
 	bg_dat <- yr_dat[!is.na(btemp)][!duplicated(paste(lon,lat))] # background data; unique lon-lat combinations, with bottom temperature
@@ -46,7 +49,8 @@ for(us in 1:1){
 					layout(lo) # set up figure layout so can add images more easily
 				}
 				if(plot_temp){
-					bg_dat[year==tyr, plot_map(x=lon, y=lat, marks=btemp, map_owin=topoFish::mapOwin[[treg]])] # plot background colors
+					zlim <- bg_dat[,range(btemp, na.rm=TRUE)]
+					bg_dat[year==tyr, plot_map(x=lon, y=lat, marks=btemp, map_owin=topoFish::mapOwin[[treg]], zlim=zlim)] # plot background colors
 				}else{
 					plot(topoFish::mapOwin[[treg]], main="")
 					map(add=TRUE, fill=TRUE, col='lightgray')
@@ -80,16 +84,25 @@ for(us in 1:1){
 				# tdat[,points(lon, lat, pch=19, col='black', cex=1.5)]
 				
 				if(ty==1){t1_dat <- tdat}
-				# t1_dat[,drawRadius(lon, lat, rsize=radius_size, col='lightgray')]
+				# t1_dat[,drawRadius(lon, lat, rsize=radius_size/2, col='lightgray')]
 				if(nrow(t1_dat)>0){
-					# t1_dat[,drawRadius(lon, lat, rsize=radius_size, border=NA, col=adjustcolor(clustColor, 0.1)),by='clustGroup']
+					# t1_dat[,drawRadius(lon, lat, rsize=radius_size/2, border=NA, col=adjustcolor(clustColor, 0.1)),by='clustGroup']
 				}
 				if(nrow(tdat)>0){
-					tdat[,drawRadius(lon, lat, rsize=radius_size, border=clustColor, col=adjustcolor(clustColor, 0.25)),by='clustGroup']
+					tdat[,drawRadius(lon, lat, rsize=radius_size/2, border=clustColor, col=adjustcolor(clustColor, 0.25)),by='clustGroup']
 					tdat[,points(lon, lat, pch=21, bg=clustColor, cex=1.5)] # plot points where the species was observed
 				}
 				mtext(tyr, side=1, line=-1, font=2, adj=0.1)
 				mtext(tspp, side=3, line=0.1, font=3, adj=0.9)
+				if(tyr%in%col_yrs & !tyr%in%ext_yrs){
+					mtext("Colonization", side=1, line=-1, adj=0.9)
+				}else if(!tyr%in%col_yrs & tyr%in%ext_yrs){
+					mtext("Extinction", side=1, line=-1, adj=0.9)
+				}else if(tyr%in%col_yrs & tyr%in%ext_yrs){
+					mtext("Colonization & Extinction", side=1, line=-1, adj=0.9)
+				}else{
+					
+				}
 				
 				if(require("trawlData")){
 					trawlData::sppImg(tspp) # plot picture of critter
