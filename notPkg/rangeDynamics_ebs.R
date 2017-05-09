4596bca44771ee4dee888c8bb6dbdcd1f48360b6
rBatt <battrd@gmail.com>
2017-05-08 20:18:09 -0400

    improve animation
    temporarily only looping through 1 spp

--- notPkg/rangeDynamics_ebs.R
+++ notPkg/rangeDynamics_ebs.R
@@ -12,6 +12,14 @@ lo <- matrix(rep(1, 9), ncol=3)
 lo[1,1] <- 2
 
 
+# ===========
+# = options =
+# ===========
+
+radius_size <- 100
+plot_convex_hull <- FALSE
+plot_temp <- TRUE
+
 uspp <- ebs_sppMaster[ce_categ=="both", unique(spp)]
 # setwd("~/Documents/School&Work/pinskyPost/topoFish/notPkg")
 old.wd <- getwd()
@@ -36,7 +44,12 @@ for(us in 1:1){
 				
 				if(require("trawlData")){
 					layout(lo) # set up figure layout so can add images more easily
-					bg_dat[year==tyr, plot_map(x=lon, y=lat, marks=btemp, map_owin=spatialDiversity::mapOwin[[treg]])] # plot background colors
+				}
+				if(plot_temp){
+					bg_dat[year==tyr, plot_map(x=lon, y=lat, marks=btemp, map_owin=topoFish::mapOwin[[treg]])] # plot background colors
+				}else{
+					plot(topoFish::mapOwin[[treg]], main="")
+					map(add=TRUE, fill=TRUE, col='lightgray')
 				}
 				
 				
@@ -47,29 +60,41 @@ for(us in 1:1){
 					ydat <- tdat[combos[,V2], list(lon,lat)]
 					dists <- as.dist(matrix(geoDist(x=xdat, y=ydat), nrow=nrtd)) # geoDist is like Euclidean, but curvature of Earth
 					clusts <- hclust(dists, method='single') # do hierarchical clustering
-					groups <- cutree(clusts, h=100) # h is the cutoff height from the dendrogram
+					groups <- cutree(clusts, h=radius_size) # h is the cutoff height from the dendrogram
 					tdat[,clustGroup:=groups]
 					tdat[,clustColor:=viridis::viridis(max(groups))[groups]]
 					
-					tdat[, j={
-						ch <- chull(x=lon, y=lat)
-						ch <- c(ch, ch[1])
-						.SD[ch,lines(list(x=lon,y=lat), col=clustColor)]
-					} ,by=c("clustGroup")]
+					if(plot_convex_hull){
+						tdat[, j={
+							ch <- chull(x=lon, y=lat)
+							ch <- c(ch, ch[1])
+							.SD[ch,lines(list(x=lon,y=lat), col=clustColor)]
+						} ,by=c("clustGroup")]
+					}
 					
 				}else{
 					tdat[,clustColor:='black']
+					tdat[,clustGroup:=1]
 				}
 				
 				# tdat[,points(lon, lat, pch=19, col='black', cex=1.5)]
 				
-				tdat[,points(lon, lat, pch=21, bg=clustColor)] # plot points where the species was observed
+				if(ty==1){t1_dat <- tdat}
+				# t1_dat[,drawRadius(lon, lat, rsize=radius_size, col='lightgray')]
+				if(nrow(t1_dat)>0){
+					# t1_dat[,drawRadius(lon, lat, rsize=radius_size, border=NA, col=adjustcolor(clustColor, 0.1)),by='clustGroup']
+				}
+				if(nrow(tdat)>0){
+					tdat[,drawRadius(lon, lat, rsize=radius_size, border=clustColor, col=adjustcolor(clustColor, 0.25)),by='clustGroup']
+					tdat[,points(lon, lat, pch=21, bg=clustColor, cex=1.5)] # plot points where the species was observed
+				}
 				mtext(tyr, side=1, line=-1, font=2, adj=0.1)
 				mtext(tspp, side=3, line=0.1, font=3, adj=0.9)
 				
 				if(require("trawlData")){
 					trawlData::sppImg(tspp) # plot picture of critter
 				}
+				t1_dat <- tdat
 			}
 	
 		},
