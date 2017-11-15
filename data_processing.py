c66b81c5d9b31c0729dcfa97c39260c57238fd20
Nash Keyes <ndbkeyes@gmail.com>
2017-11-14 22:22:23 -0500

    Basic animation
    Created basic animation of the data over time for one species using graphics.py

--- data_processing.py
+++ data_processing.py
@@ -34,6 +34,22 @@ def data_subset(data,species,year):
 
 	return result
 
+# function - plot observation point on window
+def plot(window,info=False):
+
+	p = Point(x.longitude,abs(x.latitude))
+	c = Circle(p,5)
+	if info:
+		print(x.index,x.longitude,x.latitude)
+	c.setOutline("black")
+	c.draw(window)
+
+	return c
+
+
+################################################################################
+
+
 # open and read data from file
 data = []
 with open("mydata.csv", "r") as csvfile:
@@ -43,15 +59,15 @@ with open("mydata.csv", "r") as csvfile:
 		data.append(x)
 
 # create subset of data
-species = 'none'
-year = 2014
-data_subset = data_subset(data,species,year)
+species = 'Aforia circinata'
+year = 'none'
+subset = data_subset(data,species,year)
 
 # find max and min lats and longs of Asterias amurensis observations
-min_lat = min([x.latitude for x in data_subset])
-max_lat = max([x.latitude for x in data_subset])
-min_long = min([x.longitude for x in data_subset])
-max_long = max([x.longitude for x in data_subset])
+min_lat = min([x.latitude for x in subset])
+max_lat = max([x.latitude for x in subset])
+min_long = min([x.longitude for x in subset])
+max_long = max([x.longitude for x in subset])
 
 # calculate and scale window dimensions, allow for border padding
 width = abs(max_long - min_long)*100 + 10
@@ -59,7 +75,7 @@ height = abs(max_lat - min_lat)*100 + 10
 print(width, height)
 
 # adjust coordinates
-for x in data_subset:
+for x in subset:
 	# normalize with minima
 	x.latitude -= min_lat
 	x.longitude -= min_long
@@ -75,13 +91,36 @@ for x in data_subset:
 # create graph window
 win = GraphWin("topoFish",width,height)
 
-# plot points in data selection
-for x in data_subset:
-	p = Point(x.longitude,abs(x.latitude))
-	print(x.index,x.longitude,x.latitude)
-	p.setOutline("black")
-	p.draw(win)
+# plot points for each year for this species
+for year in range(1984,2015):
+
+	scatter = []  # array to hold graphics objects plotted
+
+
+	# plot this round of points
+	year_subset = data_subset(subset,'none',year)
+	for x in year_subset:
+		scatter.append(plot(win,x))
+
+	# time delay
+	for x in range(5000):
+		Point(-10,-10).draw(win)
+
+	# undraw this round of points
+	for x in scatter:
+		x.undraw()
+
 
 # sketchy thing that keeps window open til you close it, and doesn't throw error on close
 while win.isOpen():
 	Point(-10,-10).draw(win)
+
+
+################################################################################
+
+
+# Okay I really need to get some better plotting library. Probably matplotlib is the way to go tbh and then I could not do all the scaling shite. Next step!
+# 1) transition to matplotlib
+# 2) create animations
+# 3) implement best cluster-id method
+# 4) start thinking about cluster-tracking problem
