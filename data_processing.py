74372d7e8a8e9a5b1bb1b65f50585d86c6c0a584
Nash Keyes <ndbkeyes@gmail.com>
2017-11-17 00:25:10 -0500

    Matplotlib animation, basic clustering
    Finished and cleaned up matplotlib animation of data.
    Used scipy fclusterdata to try out basic clustering of data. Need to figure out how to set cutoff parameter value so that the correct clustering is computed for all data sets.
    Currently, this program plots and attempts to cluster (with parameter 1) Aforia Circinata observations in each year.

--- data_processing.py
+++ data_processing.py
@@ -2,9 +2,11 @@ import csv
 import math
 from graphics import *
 import matplotlib.pyplot as plt
+from scipy.cluster import hierarchy
 
 # class - observation datum
 class observation(object):
+
 	def __init__(self, index, species, longitude, latitude, year):
 		self.index = int(index)
 		self.species = species
@@ -35,7 +37,58 @@ def data_subset(data,species,year):
 
 	return result
 
-################################################################################
+# function to plot one data set (all observations of one species in one year)
+def plot_set(data,species,year):
+	data_set = data_subset(data,species,year)
+	plt.scatter([x.longitude for x in data_set], [x.latitude for x in data_set])
+	plt.axis([min_long - 1, max_long + 1, min_lat - 1, max_lat + 1])
+	plt.show()
+
+# =creates animation of observations of a certain species over the years
+def animate(data):
+
+	plt.ion()  # set plotting mode to interactive or whatever
+	plt.show()
+
+	# animate data from year to year
+	i = 1
+	for year in range(1984,2015):
+
+		year_subset = data_subset(data,species,year)
+		print(year)
+
+		# plot and scale current figure
+		fig = plt.figure(i)
+		plt.scatter([x.longitude for x in year_subset], [x.latitude for x in year_subset])
+		plt.axis([min_long - 1, max_long + 1, min_lat - 1, max_lat + 1])
+
+		# draw and then clear current points
+		plt.draw()
+		plt.pause(0.1)
+		plt.clf()
+
+# find the cluster distribution for a data set
+def clusters(data,species,year,param=1):
+	radius = 100
+	points = [[x.longitude, x.latitude] for x in data_subset(data,species,year)]
+	print(len(points))
+	if len(points) > 1:
+		clusters = hierarchy.fclusterdata(points,param)
+		return clusters
+	else:
+		return 0
+
+# modified range function to allow floats - used to test out different param values in fclusterdata
+def frange(start, stop, step):
+	result = []
+	i = start
+	while i < stop:
+		yield i
+		i += step
+		result.append(i)
+	return result
+
+###############################################################################
 
 # open and read data from file
 data = []
@@ -56,22 +109,19 @@ max_lat = max([x.latitude for x in subset])
 min_long = min([x.longitude for x in subset])
 max_long = max([x.longitude for x in subset])
 
-plt.ion()  # set plotting mode to interactice
-plt.show()
+# try out different parameter values on the 1985 data set to see which works best
+"""
+for i in frange(0.1,2,0.01):
+	print(i, clusters(data,species,1985,i))
+plot_set(data,species,1985)
+"""
 
-# animate data from year to year
-i = 1
+# test the parameter value over the data over the years
+param = 1
 for year in range(1984,2015):
+	print(clusters(data,species,year,param))
+	plot_set(data,species,year)
 
-	year_subset = data_subset(data,species,year)
-	print(year)
-
-	fig = plt.figure(i)
-	plt.scatter([x.longitude for x in year_subset], [x.latitude for x in year_subset])
-	plt.axis([min_long - 1, max_long + 1, min_lat - 1, max_lat + 1])
-
-	plt.draw()
-	plt.pause(0.1)
-	plt.clf()
-
+# https://github.com/mstrosaker/hclust/wiki/User's-guide
 # https://stackoverflow.com/questions/28269157/plotting-in-a-non-blocking-way-with-matplotlib
+# https://docs.scipy.org/doc/scipy-0.19.1/reference/generated/scipy.spatial.distance_matrix.html
